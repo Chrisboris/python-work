fc4f63ce3633b698b5dc023368336fa421a11ecd
baganada <Kibuddessali@gmail.com>
2019-01-23 04:01:27 -1200

    python

---
+++ demo.py
@@ -0,0 +1,14 @@
+x = int(input("year of birth"))
+ 
+years = 2019 - x
+
+if years <= 18:
+    print ("your a minor")
+
+elif years <= 36:
+    print ("your a youth")
+
+else:
+    print ("your an elder")
+
+    
