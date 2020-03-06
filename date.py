f23837f1ee27a2c958884ac9a4d95e656cbfbd5f
Bernard <kibuddessali@gmail.com>
2020-03-06 04:48:43 -0800

    successful

---
+++ date.py
@@ -0,0 +1,8 @@
+Date = int(input("enter date of birth: "))
+Month = input("enter month of birth: ")
+Year = int(input("enter year of birth: "))
+
+Age = (2020 - Year)
+
+print("you are " + str(Age )  + " years ")
+print("You were born on " + str( Date ) +  str( Month ) + str( Year ))
